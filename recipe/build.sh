4c0d64d09d4725e981ff82b9f675f259187842d7
Thorsten Beier <derthorstenbeier@gmail.com>
2023-06-06 11:31:09 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,7 @@ cp $RECIPE_DIR/setup.py .
 
 export ARCTICDB_USING_CONDA=1
 
+# do this only when cross compiling TODO
+cp $BUILD_PREFIX/bin/protc $PREFIX/bin/protc
+
 CMAKE_BUILD_PARALLEL_LEVEL=1 $PYTHON -m pip install . -vv

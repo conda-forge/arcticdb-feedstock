2fee827640874d8c2d176ad4c4b438552014f2a5
DerThorsten <derthorstenbeier@gmail.com>
2023-06-06 13:46:54 +0200

    python in build only when cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export ARCTICDB_USING_CONDA=1
+export CMAKE_BUILD_PARALLEL_LEVEL=1
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
   cp $RECIPE_DIR/setup.py .
@@ -8,4 +9,4 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
   ln -s $BUILD_PREFIX/bin/protoc $PREFIX/bin/protoc
 fi
 
-CMAKE_BUILD_PARALLEL_LEVEL=1 $PYTHON -m pip install . -vv
+ $PYTHON -m pip install . -vv --no-build-isolation --no-deps

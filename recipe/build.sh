01f74d6f45b9bf90b459840abf1c837506e9fd26
DerThorsten <derthorstenbeier@gmail.com>
2023-06-06 13:35:21 +0200

    only copy setup.py when crosscompiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cp $RECIPE_DIR/setup.py .
-
 export ARCTICDB_USING_CONDA=1
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  cp $RECIPE_DIR/setup.py .
   rm $PREFIX/bin/protoc
   ln -s $BUILD_PREFIX/bin/protoc $PREFIX/bin/protoc
 fi

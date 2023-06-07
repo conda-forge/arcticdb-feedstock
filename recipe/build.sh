309cd0d64a2a7d1cd9e841640c1a6360609eb183
DerThorsten <derthorstenbeier@gmail.com>
2023-06-07 09:08:03 +0200

    added comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,8 @@ export ARCTICDB_USING_CONDA=1
 export CMAKE_BUILD_PARALLEL_LEVEL=1
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  # otherwise we get the wrong binary when 
+  # "compiling" the protobuf files
   rm $PREFIX/bin/protoc
   ln -s $BUILD_PREFIX/bin/protoc $PREFIX/bin/protoc
 fi

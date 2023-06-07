8d4f8ad18c871ef513bf5370b59691f29cbeb9b2
DerThorsten <derthorstenbeier@gmail.com>
2023-06-07 08:41:16 +0200

    try without setup.py fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,6 @@ export ARCTICDB_USING_CONDA=1
 export CMAKE_BUILD_PARALLEL_LEVEL=1
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  cp $RECIPE_DIR/setup.py .
   rm $PREFIX/bin/protoc
   ln -s $BUILD_PREFIX/bin/protoc $PREFIX/bin/protoc
 fi

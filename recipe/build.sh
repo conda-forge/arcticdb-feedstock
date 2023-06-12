cd5a06035b80a1f82966bbf0d3c4a52fbf49eb10
Julien Jerphanion <git@jjerphan.xyz>
2023-06-12 09:01:16 +0200

    docs: Document latest PRs' changes
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 export ARCTICDB_USING_CONDA=1
+# Compiling ArcticDB with all cores might freeze machines due to swapping.
+# We build with only 1 core to prevent these freezes from happening.
 export CMAKE_BUILD_PARALLEL_LEVEL=1
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  # otherwise we get the wrong binary when 
-  # "compiling" the protobuf files
+  # We have to `protoc` from the build instead of the one from the host
+  # in order to get a usable binary to compile the protobuf files.
   rm $PREFIX/bin/protoc
   ln -s $BUILD_PREFIX/bin/protoc $PREFIX/bin/protoc
 fi
 
- $PYTHON -m pip install . -vv --no-build-isolation --no-deps
+$PYTHON -m pip install . -vv --no-build-isolation --no-deps

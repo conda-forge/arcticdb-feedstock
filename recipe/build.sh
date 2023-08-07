a128cd9bcc59bdddb7fb1058e48f7b9e8dee6d33
DerThorsten <derthorstenbeier@gmail.com>
2023-08-07 08:58:29 +0200

    use a custom CMakePresets.json file

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,10 @@ export ARCTICDB_USING_CONDA=1
 export CMAKE_BUILD_PARALLEL_LEVEL=1
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  # Get an updated config.sub and config.guess
+
+
+
+  # Get an updated config.sub and config.guess  
   # (see https://conda-forge.org/docs/maintainer/knowledge_base.html#cross-compilation)
   cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
@@ -14,6 +17,19 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
   # in order to get a usable binary to compile the protobuf files.
   rm $PREFIX/bin/protoc
   ln -s $BUILD_PREFIX/bin/protoc $PREFIX/bin/protoc
+
+  # if we cross compile for apple 
+  if [[ "$target_platform" == "osx-arm64" ]]; then
+    echo "Cross compiling for apple silicon"
+
+    # when cross compiling for apple silicon, we absolutely need to make sure
+    # that there is no hard coded compiler in the cmake-preset / cmake-config.
+    # while https://github.com/man-group/ArcticDB/pull/662 is not merged, we
+    # need use a custom cmake preset file for apple silicon.
+    cp $RECIPE_DIR/CMakePresetCrossCompileAppleSilicon.json $SRC_DIR/cpp/CMakePresets.json
+
+  fi
+
 fi
 
 $PYTHON -m pip install . -vv --no-build-isolation --no-deps

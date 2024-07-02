f3de90ad1fc80ee20e653c3eb20d1bb671a80a01
Julien Jerphanion <git@jjerphan.xyz>
2024-07-02 09:25:00 +0200

    Only use 1 thread
    Compilation seems to hang around: `version/symbol_list.cpp`
    
    See: https://dev.azure.com/conda-forge/feedstock-builds/_build/results?buildId=967730&view=logs&j=80cb4bd3-7e0a-50cc-19d9-637946946f1a&t=046bad2a-bca6-54c7-3f51-632649b9ec75
    
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,10 @@ export ARCTICDB_USING_CONDA=1
 # around binary operators' template specialization expansion and compilation.
 # See: https://github.com/man-group/ArcticDB/blob/master/cpp/arcticdb/processing/operation_dispatch_binary.hpp#L451-L473
 #
+# It also has been observed to hang on `version/symbol_list.cpp`.
 # The feedstock's machine have 6.759GiB of RAM and 2 (physical?) cores so we cap it to that.
-# We build with only 2 threads to prevent these freezes from happening.
-export CMAKE_BUILD_PARALLEL_LEVEL=2
+# We build with only 1 thread to prevent these freezes from happening.
+export CMAKE_BUILD_PARALLEL_LEVEL=1
 
 # Required to be able to include headers from glog since glog 0.7
 # See: https://github.com/google/glog/pull/1030

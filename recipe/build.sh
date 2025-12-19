f0c4aa225a54d2f27f3d5091a18ed139dfe60373
Julien Jerphanion <git@jjerphan.xyz>
2025-12-19 09:52:25 +0100

    Use all threads
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 export ARCTICDB_USING_CONDA=1
-# Compiling ArcticDB with all cores might freeze machines due to swapping.
-# We build with only 1 core to prevent these freezes from happening.
-export CMAKE_BUILD_PARALLEL_LEVEL=1
 
 # Workaround for: https://github.com/conda-forge/glog-feedstock/issues/26
 # Required to be able to include headers from glog since glog 0.7.
